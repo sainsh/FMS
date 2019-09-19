a3a72380e45840fba510a03667addfe5ba89a6db
sainsh <kfsainsh@gmail.com>
2019-09-19 14:20:22 +0200

    test 2 GREEN BAR

--- src/fms/ContainerShip.java
+++ src/fms/ContainerShip.java
@@ -1,10 +1,14 @@
 package fms;
 
 public class ContainerShip {
+
+    private final String name = "Marco Polo";
+
     public ContainerShip(String name) {
         
     }
 
     public String getName() {
+        return name;
     }
 }
