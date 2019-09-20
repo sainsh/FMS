317c50ac7a21628f961a1785a7c37c12b8f06eb0
sainsh <kfsainsh@gmail.com>
2019-09-20 09:39:28 +0200

    implemented test case 11

--- test/fms/TankerTest.java
+++ test/fms/TankerTest.java
@@ -7,7 +7,7 @@ import java.time.LocalDate;
 
 public class TankerTest extends TestCase {
 
-    public void testCreateTanker(){
+    public void testCreateTankerFakeIt(){
 
         final String name = "Seawise Giant";
         final LocalDate launchDate = LocalDate.of(1979,12,4);
@@ -23,6 +23,25 @@ public class TankerTest extends TestCase {
         assertEquals(tonnageVolume, tanker.getTonnageVolume());
         assertEquals(status, tanker.getStatus());
 
+
+
+    }
+
+    public void testCreateTanker(){
+
+        final String name = "Exxon Valdez";
+        final LocalDate launchDate = LocalDate.of(1986,10,14);
+        final int dwt = 214861;
+        final int tonnageVolume = 0;
+        final Status status = Status.DockedAtHome;
+
+        Tanker tanker = new Tanker(name, launchDate,dwt,tonnageVolume,status);
+
+        assertEquals(name, tanker.getName());
+        assertEquals(launchDate, tanker.getLaunchDate());
+        assertEquals(dwt, tanker.getDeadWeightTonnage());
+        assertEquals(tonnageVolume, tanker.getTonnageVolume());
+        assertEquals(status, tanker.getStatus());
 
 
     }
